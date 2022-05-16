2476a91ed0918fd8a7ff9d2ecc3995888321fa68
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2022-05-16 16:51:35 -0400

    Integrate Jack's repo

---
+++ api/models/Account.js
@@ -0,0 +1,25 @@
+const { Model } = require("objection");
+const { hash, compare } = require("bcrypt");
+
+const SALT_ROUNDS = 10;
+
+class Account extends Model {
+  static get tableName() {
+    return "account";
+  }
+
+  // Encrypt the password before storing it in the database.
+  // SHOULD ALSO DO THIS ON UPDATE!
+
+  // eslint-disable-next-line no-unused-vars
+  async $beforeInsert(queryContext) {
+    this.password = await hash(this.password, SALT_ROUNDS);
+  }
+
+  async verifyPassword(plainTextPassword) {
+    console.log(`compare(${plainTextPassword}, ${this.password})`);
+    return compare(plainTextPassword, this.password);
+  }
+}
+
+module.exports = Account;
