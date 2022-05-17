654ccb64314a37dc93016f843ce318c176378ac4
jack_hammond@taylor.edu <jhammond@cse21811.cs.local>
2022-05-16 20:19:26 -0400

    commit page test

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,7 +3,20 @@
     <div>
       <h4 class="display-1">About Us</h4>
 
-      <p class="body-1">We are a bunch of nerds.</p>
+      <p class="body-1">Creating Carpool Community since 2022</p>
+    </div>
+    <div style="background-color:#C8E6C9" class="px-10">
+      <h1>FAQs</h1>
+      <br>
+      <h3>What areas do you service?</h3>
+      <p>Ride Share currently services the continental US, but we are always taking new drivers!</p>
+      <br>
+      <h3>Who can become a driver?</h3>
+      <p>Anyone with an authorized license and vehicle can become a driver.</p>
+      <br>
+      <h3>How can I see what rides are offered?</h3>
+      <p>Head to our Ride page <a href="http://localhost:8080/rides">here</a> to look at all of our current rides.</p>
+      <br>
     </div>
   </v-container>
 </template>
